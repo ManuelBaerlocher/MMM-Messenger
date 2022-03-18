00b97eef0d617a38786624292c93c48e5845f7ea
Manuelb1990 <manuell@bluewin.ch>
2022-03-18 18:26:42 +0100

    add angular material and firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'mmm-messenger',
+    appId: '1:912173635802:web:89a5fae83f9cc0d5e72de3',
+    storageBucket: 'mmm-messenger.appspot.com',
+    apiKey: 'AIzaSyA4y84jzKxGszib01oT_kpiz5e8K_YWJnM',
+    authDomain: 'mmm-messenger.firebaseapp.com',
+    messagingSenderId: '912173635802',
+  },
   production: false
 };
 
