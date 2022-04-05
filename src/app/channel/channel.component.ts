58974fd523d92f524e218fab7d2fb70e505cd34b
Manuelb1990 <manuell@bluewin.ch>
2022-04-05 19:29:30 +0200

    add diferent changes

--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { FormControl } from '@angular/forms';
 import { TooltipPosition } from '@angular/material/tooltip';
@@ -29,108 +30,118 @@ export class ChannelComponent implements OnInit {
     private route: ActivatedRoute,
     public firestore: AngularFirestore,
     public userService: UserService,
+    public auth: AngularFireAuth
   ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(paramMap => {
       this.channelId = paramMap.get('id');
+      this.checkCurrentUser();
       this.getChannel();
       this.getPost();
     })
 
   }
 
-  getPost() {
-    this.firestore
-      .collection('channels')
-      .doc(this.channelId)
-      .collection('posts', ref =>
-        ref.orderBy('time', 'asc'))
-      .valueChanges({ idField: 'customIdName' })
-      .subscribe((changes: any) => {
-        this.allPosts = changes;
-      })
-  }
-
-  getChannel() {
-    this.firestore
-      .collection('channels')
-      .doc(this.channelId)
-      .valueChanges()
-      .subscribe((channel: any) => {
-        this.channel = new Channel(channel);
-      })
-  }
-
-  newPost() {
-    this.post.userId = this.authService.currentUserId
-    this.checkUser();
-    this.checkDate();
-
-    this.firestore
-      .collection('channels')
-      .doc(this.channelId)
-      .collection('posts')
-      .add(this.post.toJSON());
-    this.post.content = ''
-  }
-
-  checkUser() {
-    this.post.userId = this.authService.currentUserId
-    for (let i = 0; i < this.userService.allUsers.length; i++) {
-      let uid: string = this.userService.allUsers[i].uid;
-
-      if (this.post.userId == uid) {
-        this.post.userName = this.userService.allUsers[i].displayName
-        this.checkUserImg(i)
-      }
-    }
-  }
-
-  checkUserImg(i) {
-    if (this.userService.allUsers[i].photoURL == undefined) {
-      this.post.userImg = 'assets/img/user-placeholder.png';
-    } else {
-      this.post.userImg = this.userService.allUsers[i].photoURL;
-    }
-  }
-
-  checkDate() {
-    const formatDate = new Intl.DateTimeFormat("de", {
-      day: "2-digit",
-      month: "2-digit",
-      year: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
+  checkCurrentUser() {
+    this.auth.currentUser.then((result: any) => {
+      console.log('user', result.uid)
+      
     });
 
-    this.post.time = formatDate.format(new Date())
   }
 
-  answerToPost(id) {
-    this.post.userId = this.authService.currentUserId
-    this.checkUser();
-    this.checkDate();
+    getPost() {
+      this.firestore
+        .collection('channels')
+        .doc(this.channelId)
+        .collection('posts', ref =>
+          ref.orderBy('time', 'asc'))
+        .valueChanges({ idField: 'customIdName' })
+        .subscribe((changes: any) => {
+          this.allPosts = changes;
+        })
+    }
 
-    this.firestore
-      .collection('channels')
-      .doc(this.channelId)
-      .collection('posts')
-      .doc(id)
-      .collection('answers')
-      .add(this.post.toJSON());
-    this.post.content = ''
-  }
+    getChannel() {
+      this.firestore
+        .collection('channels')
+        .doc(this.channelId)
+        .valueChanges()
+        .subscribe((channel: any) => {
+          this.channel = new Channel(channel);
+        })
+    }
 
-  deletePost(id) {
-    this.firestore
-      .collection('channels')
-      .doc(this.channelId)
-      .collection('posts')
-      .doc(id)
-      .delete()
-      .then(res => {
-      })
+    newPost() {
+      this.post.userId = this.authService.currentUserId
+      this.checkUser();
+      this.checkDate();
+
+      this.firestore
+        .collection('channels')
+        .doc(this.channelId)
+        .collection('posts')
+        .add(this.post.toJSON());
+      this.post.content = ''
+    }
+
+    checkUser() {
+      this.post.userId = this.authService.currentUserId
+      for (let i = 0; i < this.userService.allUsers.length; i++) {
+        let uid: string = this.userService.allUsers[i].uid;
+
+        if (this.post.userId == uid) {
+          this.post.userName = this.userService.allUsers[i].displayName
+          this.checkUserImg(i)
+        }
+      }
+    }
+
+    checkUserImg(i) {
+      if (this.userService.allUsers[i].photoURL == undefined) {
+        this.post.userImg = 'assets/img/user-placeholder.png';
+      } else {
+        this.post.userImg = this.userService.allUsers[i].photoURL;
+      }
+    }
+
+    checkDate() {
+      const formatDate = new Intl.DateTimeFormat("de", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      });
+
+      this.post.time = formatDate.format(new Date())
+    }
+
+    answerToPost(id) {
+      this.post.userId = this.authService.currentUserId
+      this.checkUser();
+      this.checkDate();
+
+      this.firestore
+        .collection('channels')
+        .doc(this.channelId)
+        .collection('posts')
+        .doc(id)
+        .collection('answers')
+        .add(this.post.toJSON());
+      this.post.content = ''
+    }
+
+    deletePost(id) {
+      this.firestore
+        .collection('channels')
+        .doc(this.channelId)
+        .collection('posts')
+        .doc(id)
+        .delete()
+        .then(res => {
+        })
+    }
   }
-}
